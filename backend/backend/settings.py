fe7988f88026c38c2a4e54c91179920af89a11fa
muhammedfazal1122 <muhammedfazalcholayil@gmail.com>
2024-04-30 21:21:06 +0530

    added all the requiremnet file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -16,6 +16,8 @@ import os
 from datetime import timedelta
 from decouple import config
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
