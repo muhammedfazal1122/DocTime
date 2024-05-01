be4f5051485bd4f84b4a95ed540949d77798df38
muhammedfazal1122 <muhammedfazalcholayil@gmail.com>
2024-05-01 20:35:23 +0530

    pycopg3 4

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -119,8 +119,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DATABASE_NAME'),
-        'USER': config('DATABASE_USER'),
+        'NAME': 'doctime',
+        'USER': 'ubuntu',
         'PASSWORD': config('DATABASE_PASS'),
         'HOST': 'localhost',  # Or the host where your PostgreSQL server is running
         'PORT': '5432',       # Default PostgreSQL port
