80eae4e069fb47b4f4517e97497d6f418ccc3a20
muhammedfazal1122 <muhammedfazalcholayil@gmail.com>
2024-05-01 19:47:29 +0530

    psycopg2 3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -118,7 +118,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config('DATABASE_NAME'),
         'USER': config('DATABASE_USER'),
         'PASSWORD': config('DATABASE_PASS'),
