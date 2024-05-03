c6af24f7935e3e3d7b3b997baeadb33e6d0f27b0
muhammedfazal1122 <muhammedfazalcholayil@gmail.com>
2024-05-03 18:14:05 +0530

    account undo

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -181,7 +181,7 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-AUTH_USER_MODEL = 'User'
+AUTH_USER_MODEL = 'accounts.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
