663e289a9a373fc6745ffd4d7bebf30e4fbcc7b6
muhammedfazal1122 <muhammedfazalcholayil@gmail.com>
2024-05-03 10:01:41 +0530

    added account.

---
+++ backend/accounts/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+
+
+admin.site.register(User)
+admin.site.register(OTPModel)
+admin.site.register(Doctor)
+admin.site.register(Patient)
