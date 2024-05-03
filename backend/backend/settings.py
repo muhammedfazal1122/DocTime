e0eb8fb8584d4a54788f5ba79c0eaad929735674
muhammedfazal1122 <muhammedfazalcholayil@gmail.com>
2024-05-03 19:54:06 +0530

    Deleted all the migratie files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -167,10 +167,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    'django_rest_backend/static',
-]
-   
    
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
